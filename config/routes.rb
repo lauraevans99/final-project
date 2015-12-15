9cd5c19e7d8a1d37b4ed657e3be3245bc763bf83
lauraevans99 <lauraevans99@gmail.com>
2015-12-15 14:43:40 -0800

    added basket

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   mount Shoppe::Engine => "/shoppe"
 
+  root to: "products#index"
   get "product/:permalink", to: "products#show", as: "product"
   post "product/:permalink", to: "products#buy", as: "buy"
-  root to: "products#index"
-
+  get "basket", to: "orders#show"
+  delete "basket", to: "orders#destroy"
+  
 end

1cb1c826bf7aef9257c8a0d54de5719ff34753eb
lauraevans99 <lauraevans99@gmail.com>
2015-12-15 18:23:06 -0800

    orders can be sumitted

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,8 +15,13 @@ class OrdersController < ApplicationController
 	    if @order
 
 	      puts "in patch2"
-	      @order.proceed_to_confirm(params[:order].permit(:first_name, :last_name, :billing_address1, :billing_address2, :billing_address3, :billing_address4, :billing_country_id, :billing_postcode, :email_address, :phone_number))
+	      # @order.proceed_to_confirm(params[:order].permit(:first_name, :last_name, :billing_address1, :billing_address2, :billing_address3, :billing_address4, :billing_country_id, :billing_postcode, :email_address, :phone_number))
+	      @params = params[:order].permit(:first_name, :last_name, :billing_address1, :billing_address2, :billing_address3, :billing_address4, :billing_country_id, :billing_postcode, :email_address, :phone_number)
+	      @order.update(@params)
+
 	      p @order
+	      puts "BREAK............."
+	      p @order.order_items
 	      puts "in patch3"
 
 	      redirect_to checkout_payment_path
@@ -25,9 +30,11 @@ class OrdersController < ApplicationController
 	end
 
 	def payment
-	  @order = Shoppe::Order.find(session[:current_order_id])
+	  @order = Shoppe::Order.find(current_order.id)
+	  p @order
 	  if request.post?
 	    if @order.accept_stripe_token(params[:stripe_token])
+	    	p @order
 	      redirect_to checkout_confirmation_path
 	    else
 	      flash.now[:notice] = "Could not exchange Stripe token. Please try again."
@@ -37,6 +44,7 @@ class OrdersController < ApplicationController
 
 	def confirmation
 	  if request.post?
+	  	p @order
 	    current_order.confirm!
 	    session[:order_id] = nil
 	    redirect_to root_path, :notice => "Order has been placed successfully!"
